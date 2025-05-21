40e83bffbba6ca5e37a6b2bc0d7c3c8cc1a92cf9
rosalinda <perezvelayuo@gmail.com>
2025-05-21 16:12:37 -0600

    Se inicion la version, 1,2 3 del juego del soldiers_vs_aliens

--- Soldiers_vs_aliens/version_03/Goldier.py
+++ Soldiers_vs_aliens/version_03/Goldier.py
@@ -1,8 +1,19 @@
 import pygame
+from pexpect.screen import screen
 from pygame.sprite import Sprite
 from configurations import Configurations
 
-class Soldier:
-    def __init__(self):
+class Soldier(Sprite):
+    def __init__(self,sprite:Sprite):
+        super().__init__()
         soldier_image=Configurations.get_soldado_image_path()
         self.image=pygame.image.load(Configurations.get_soldado_image_path())
+
+
+    def blit(self,screen:pygame.surface):
+        """
+
+        :param screen:
+        :return:
+        """
+        screen.blit.image
